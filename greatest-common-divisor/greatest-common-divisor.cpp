54f6175360a4e17f7a9acf854863f984149710af
edo-edo <ediberidze99@gmail.com>
2021-08-01 16:16:44 +0400

    Greatest common divisor

---
+++ greatest-common-divisor/greatest-common-divisor.cpp
@@ -0,0 +1,20 @@
+// Euclid’s algorithm for finding the greatest common divisor of
+// two numbers
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    int r = a % b;
+    return gcd(b, r);
+}
+
+int main() {
+    int x, y;
+    cout << "Enter a first number: ";
+    cin >> x;
+    cout << "Enter a first second: ";
+    cin >> y;
+    cout << "Answer is: " << gcd(x, y);
+}
+
