feea1be5b2dc160014892153f9446c95dad7ccb9
edo-edo <ediberidze99@gmail.com>
2021-08-19 23:22:22 +0400

    Search an element in a reverse sorted array

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -12,11 +12,35 @@ def binary_search(element, elements):
     return -1
 
 
+def binary_search_dec(element, elements):
+    low = 0
+    high = len(elements) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if element < elements[mid]:
+            low = mid + 1
+        elif element > elements[mid]:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
 if __name__ == "__main__":
     elements_list = [-2, 1, 3, 5, 6, 12, 20]
+    elements_list_dec = [20, 12, 6, 5, 3, 1, -2]
     ELEMENT = 12
     index = binary_search(ELEMENT, elements_list)
+    index_dec_list = binary_search_dec(ELEMENT, elements_list_dec)
+
+    print(elements_list, 'Order by asc')
     if index != -1:
         print(f"Index of {ELEMENT} is: {index}")
+    else:
+        print(f"{ELEMENT} not found")
+
+    print(elements_list_dec, 'Order by dec')
+    if index_dec_list != -1:
+        print(f"Index of {ELEMENT} is: {index_dec_list}")
     else:
         print(f"{ELEMENT} not found")
